90c02f5f8fdf2b3330cfec6d84dace66145369a3
Alex Staroselsky <alexstaroselsky@live.com>
2015-11-27 19:07:28 +0000

    Created main app.scss file. Created styles task with compile, autoprefix, size, minify, and rename.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,37 @@
 var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+var del = require('del');
+var path = require('path');
+
+var config = {
+   src: {
+       javascripts: "./src/javascripts/**/*.js",
+       sass: "./src/sass/**/*.scss",
+       images: "./src/images/**/*",
+       html: "./src/html/**/*.html"
+   },
+   dist: {
+       css: "./dist/css",
+       javascripts: "./dist/javascripts",
+       images: "./dist/images"
+   }
+};
+
+gulp.task('styles', function(){
+    return gulp.src(config.src.sass)
+        .pipe(plugins.sass().on('error', plugins.sass.logError))
+        .pipe(plugins.autoprefixer())
+        .pipe(plugins.size({
+            showFiles: true
+        }))
+        .pipe(gulp.dest(config.dist.css))
+        .pipe(plugins.minifyCss())
+        .pipe(plugins.rename({ suffix: ".min" }))
+        .pipe(plugins.size({
+            showFiles: true
+        }))
+        .pipe(gulp.dest(config.dist.css));
+});
 
 gulp.task('default', function() {
   // place code for your default task here
