2a6b96ad67fc6ceab0f4d65b5b885ffeddd57fb7
Alex Staroselsky <alexstaroselsky@gmail.com>
2015-11-29 18:19:57 -0600

    Updated scripts task to concat scripts files, jquery first, then uglify/minify. Files can be added/removed to vendor and/or bootstrap folder to include/exclude files.

--- gulpfile.js
+++ gulpfile.js
@@ -4,20 +4,23 @@ var del = require('del');
 var path = require('path');
 
 var config = {
-   src: {
-       javascripts: "./src/javascripts/**/*.js",
-       sass: "./src/sass/**/*.scss",
-       images: "./src/images/**/*",
-       html: "./src/html/**/*.html"
-   },
-   dist: {
-       css: "./dist/css",
-       javascripts: "./dist/javascripts",
-       images: "./dist/images"
-   }
+    src: {
+        javascripts: {
+            bootstrap: "./src/javascripts/bootstrap/**/*.js",
+            vendor: "./src/javascripts/vendor/**/*.js"
+        },
+        sass: "./src/sass/**/*.scss",
+        images: "./src/images/**/*",
+        html: "./src/html/**/*.html"
+    },
+    dist: {
+        css: "./dist/css",
+        javascripts: "./dist/javascripts",
+        images: "./dist/images"
+    }
 };
 
-gulp.task('styles', ['clean:css'], function(){
+gulp.task('styles', ['clean:css'], function () {
     return gulp.src(config.src.sass)
         .pipe(plugins.sass().on('error', plugins.sass.logError))
         .pipe(plugins.autoprefixer())
@@ -26,31 +29,49 @@ gulp.task('styles', ['clean:css'], function(){
         }))
         .pipe(gulp.dest(config.dist.css))
         .pipe(plugins.minifyCss())
-        .pipe(plugins.rename({ suffix: ".min" }))
+        .pipe(plugins.rename({ 
+            suffix: ".min" 
+        }))
         .pipe(plugins.size({
             showFiles: true
         }))
         .pipe(gulp.dest(config.dist.css));
 });
 
-gulp.task('clean:css', function(){
+gulp.task('clean:css', function () {
     del.sync(['./dist/css/**/*.css'])
 });
 
 
-gulp.task('scripts', ['clean:js'], function(){
-    console.log("test");
+gulp.task('scripts', ['clean:js'], function () {
+    return gulp.src([
+        config.src.javascripts.vendor,
+        config.src.javascripts.bootstrap
+    ])
+    .pipe(plugins.concat('app.js'))
+    .pipe(plugins.size({
+        showFiles: true
+    }))
+    .pipe(gulp.dest(config.dist.javascripts))
+    .pipe(plugins.uglify())
+    .pipe(plugins.rename({ 
+        suffix: ".min" 
+    }))
+    .pipe(plugins.size({
+        showFiles: true
+    }))
+    .pipe(gulp.dest(config.dist.javascripts));
 });
 
-gulp.task('clean:js', function(){
+gulp.task('clean:js', function () {
     del.sync(['./dist/javascripts/**/*.js'])
 });
 
-gulp.task('html:aria', function() {
-  return gulp.src(config.src.html)
-    .pipe(plugins.accessibility());
+gulp.task('html:aria', function () {
+    return gulp.src(config.src.html)
+        .pipe(plugins.accessibility());
 });
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('default', function () {
+    // place code for your default task here
 });
